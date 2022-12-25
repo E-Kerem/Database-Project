9959af010ee41a1d5fe90dacfa6aedd060dcd074
Mert <cemil.ozdemir@ug.bilkent.edu.tr>
2022-12-25 18:35:05 +0300

    view eklendi

--- home.php
+++ home.php
@@ -16,26 +16,35 @@ session_start();
 <a href="purchasedBooks.php">Purchased books</a> <a href="addmoney.php">Add money</a> <a href="index.php">Logout</a> <a href="deleteaccount.php"> Delete my account</a>
 <br>
 <table>
-    <?php $sql = "SELECT B.book_id, B.title, G.genre_name, B.rating, EB.price  FROM book B, genre G, belongs BE, ebook EB WHERE BE.book_id = B.book_id AND G.genre_id = BE.genre_id AND EB.book_id = B.book_id ";
+    <?php
+//    $sql = "CREATE VIEW book_view AS
+//SELECT B.book_id, B.title, G.genre_name, B.rating, EB.price
+//FROM book B, genre G, belongs BE, ebook EB
+//WHERE BE.book_id = B.book_id AND G.genre_id = BE.genre_id AND EB.book_id = B.book_id;";
+//
+//    $result =  mysqli_query($conn, $sql );
 
-    $result =  mysqli_query($conn, $sql );
+
+    $sql = "SELECT * FROM book_view";
+    $result = mysqli_query($conn, $sql);
+
+    echo "<table>";
+while ($row = $result->fetch_assoc()) {
+  $bookid = $row["book_id"];
+  $booktitle = $row["title"];
+  $genrename = $row["genre_name"];
+  $bookrating = $row["rating"];
+  $bookprice = $row["price"];
+  echo "<tr><td>Book title: ", $row["title"], "</td>
+                    <td>Book genre: ", $row["genre_name"], "</td>
+                    <td>Book Rating : ", $row['rating'], "</td>
+                    <td>Price: $", $row['price'], "</td>
+                    <td><a href='reviewbook.php?booktitle=", $booktitle, "&genrename=",$genrename, "&bookrating=", $bookrating, "&bookprice=", $bookprice, "&bookid=", $bookid, "'>   Review</a></td>
+                </tr>";
+}
+echo "</table>";
 
 
-
-    if ($result->num_rows > 0 ){
-        while ($row = $result-> fetch_assoc()){
-            $bookid = $row["book_id"];
-            $booktitle = $row["title"];
-            $genrename = $row["genre_name"];
-            $bookrating = $row["rating"];
-            $bookprice = $row["price"];
-            echo "<tr><td>Book title: ", $row["title"], "</td>
-                            <td>Book genre: ", $row["genre_name"],"<td> Book Rating : ", $row['rating'],"<td> Price: $", $row['price'],  "<a href='reviewbook.php?booktitle=", $booktitle, "&genrename=",$genrename, "&bookrating=", $bookrating, "&bookprice=", $bookprice, "&bookid=", $bookid, "'>   Review</a>
-</td> </tr>  ";
-        }
-    }else {
-        echo "No Results";
-    }
     ?>
 </table>
 <form action="publishbook.php" method="post">
